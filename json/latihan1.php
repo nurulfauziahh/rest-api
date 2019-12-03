6d00be8a20d43dd43da12bc45fffc53c1486b604
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-12-03 21:25:41 +0700

    mencoba json_decode

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,12 +1,26 @@
 <?php 
 
 	
-	$mahasiswa = [
+// 	$mahasiswa =[
+// 		[
 
-			"nama" => "nurul",
-			"nrp" => "173040092",
-			"email" => "nurul@mail"
-	];
+// 			"nama" => "nurul",
+// 			"nrp" => "173040092",
+// 			"email" => "nurul@mail"
+// 		],
+// 		[
+
+// 			"nama" => "Fauziah",
+// 			"nrp" => "17304992",
+// 			"email" => "fauziah@mail"
+// 		],
+
+// ];
+
+	$dbh = new PDO('mysql:host=localhost;dbname=phpmvc', 'root', '');
+	$db = $dbh->prepare('SELECT * FROM mahasiswa');
+	$db->execute();
+	$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 	$data = json_encode($mahasiswa);
 	echo $data;
